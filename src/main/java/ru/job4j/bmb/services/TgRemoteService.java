b9e12db8ddf7b9dd4e048505cac80a8cea1d952f
AlexNosovZ <zzlifnr@gmail.com>
2026-04-06 22:48:11 +0300

    @Component и @Bean | Рефакторинг класса TgServiceRemote [#505305]

--- src/main/java/ru/job4j/bmb/services/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/services/TgRemoteService.java
@@ -10,6 +10,7 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMa
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.MoodRepository;
 import ru.job4j.bmb.repository.UserRepository;
 //import java.util.*;
 import java.util.Map;
@@ -19,26 +20,19 @@ import java.util.ArrayList;
 
 @Service
 public class TgRemoteService extends TelegramLongPollingBot {
-    private static final Map<String, String> MOOD_RESP = new LinkedHashMap<>();
-
-    static {
-        MOOD_RESP.put("lost_sock", "Носки — это коварные создания. Но не волнуйся, второй обязательно найдётся!");
-        MOOD_RESP.put("cucumber", "Огурец тоже дело серьёзное! Главное, не мариноваться слишком долго.");
-        MOOD_RESP.put("dance_ready", "Супер! Танцуй, как будто никто не смотрит. Или, наоборот, как будто все смотрят!");
-        MOOD_RESP.put("need_coffee", "Кофе уже в пути! Осталось только подождать... И ещё немного подождать...");
-        MOOD_RESP.put("sleepy", "Пора на боковую! Даже супергерои отдыхают, ты не исключение.");
-    }
-
     private final String botName;
     private final String botToken;
     private final UserRepository userRepository;
+    private TgUI tgUI;
 
     public TgRemoteService(@Value("${telegram.bot.name}") String botName,
                            @Value("${telegram.bot.token}") String botToken,
-                           UserRepository userRepository) {
+                           UserRepository userRepository,
+                           MoodRepository moodRepository) {
         this.botName = botName;
         this.botToken = botToken;
         this.userRepository = userRepository;
+        this.tgUI = new TgUI(moodRepository);
     }
 
     @Override
@@ -79,25 +73,8 @@ public class TgRemoteService extends TelegramLongPollingBot {
         message.setChatId(chatId);
         message.setText("Как настроение сегодня?");
 
-        var inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-
-        keyboard.add(List.of(createBtn("Потерял носок \uD83D\uDE22", "lost_sock")));
-        keyboard.add(List.of(createBtn("Как огурец на полке \uD83D\uDE10", "cucumber")));
-        keyboard.add(List.of(createBtn("Готов к танцам \uD83D\uDE04", "dance_ready")));
-        keyboard.add(List.of(createBtn("Где мой кофе?! \uD83D\uDE23", "need_coffee")));
-        keyboard.add(List.of(createBtn("Слипаются глаза \uD83D\uDE29", "sleepy")));
-
-        inlineKeyboardMarkup.setKeyboard(keyboard);
+        var inlineKeyboardMarkup = tgUI.buildButtons();
         message.setReplyMarkup(inlineKeyboardMarkup);
-
         return message;
-    }
-
-    InlineKeyboardButton createBtn(String name, String data) {
-        var inline = new InlineKeyboardButton();
-        inline.setText(name);
-        inline.setCallbackData(data);
-        return inline;
     }
 }
